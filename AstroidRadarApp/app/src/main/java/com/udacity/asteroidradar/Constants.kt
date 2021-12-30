29c5a1fd43c0811313dd6b5d7a8f6eca45982f6a
SefakKahriman <sefak.kahriman@gsa.gov>
2021-12-29 23:08:40 -0500

    added api key to the project

--- AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,6 +1,10 @@
 package com.udacity.asteroidradar
 
 object Constants {
+    const val API_KEY = "3DqKevLhsid80oZMvqwGETPyOY8pK5cVNuUlRXr8"
+
+    // https://api.nasa.gov/planetary/apod?api_key=YOUR_API_KEY
+    const val IMAGE_OF_THE_DAY_URL = "https://api.nasa.gov/planetary/apod"
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"

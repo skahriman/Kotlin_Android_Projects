9c676025c2dcc74646ba9012b3d387a605e04662
SefakKahriman <sefak.kahriman@gsa.gov>
2021-12-30 22:39:11 -0500

    able to get PictureOfDay properties

---
+++ AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
@@ -0,0 +1,34 @@
+package com.udacity.asteroidradar.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.PictureOfDay
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://api.nasa.gov/planetary/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NasaApiService {
+    @GET("apod")
+    suspend fun getPictureOfDayProperties(@Query("api_key") apiKey: String): PictureOfDay
+}
+
+object NasaApi {
+    val retrofitService: NasaApiService by lazy {
+        retrofit.create(NasaApiService::class.java)
+    }
+}
--- AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,41 @@
 package com.udacity.asteroidradar.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.api.NasaApi
+import com.udacity.asteroidradar.api.NasaApiService
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
+    private val API_KEY ="3DqKevLhsid80oZMvqwGETPyOY8pK5cVNuUlRXr8"
+    private val _response = MutableLiveData<String>()
+    val response: LiveData<String>
+        get() = _response
+
+    private val _propertyOfPictureOfDay = MutableLiveData<PictureOfDay>()
+    val propertyOfPictureOfDay: LiveData<PictureOfDay>
+        get() = _propertyOfPictureOfDay
+
+    init {
+        getNasaPictureOfDay()
+    }
+
+    private fun getNasaPictureOfDay() {
+        viewModelScope.launch {
+            try {
+                var listResult = NasaApi.retrofitService.getPictureOfDayProperties(API_KEY)
+                _response.value = "Success: ${listResult} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
+            }
+        }
+    }
 }

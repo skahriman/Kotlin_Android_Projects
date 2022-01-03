bdbb8c8f9002c91cfc32c5978a518e0ae969f892
SefakKahriman <sefak.kahriman@gsa.gov>
2022-01-03 05:19:55 -0500

    able to get list of astroid size

--- AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
+++ AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
@@ -1,9 +1,12 @@
 package com.udacity.asteroidradar.api
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.PictureOfDay
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -11,7 +14,9 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val BASE_URL = "https://api.nasa.gov/planetary/"
+private const val BASE_URL = "https://api.nasa.gov/"
+
+//https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -22,13 +27,29 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
+private val retrofit2 = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
 interface NasaApiService {
-    @GET("apod")
+    @GET("planetary/apod")
     suspend fun getPictureOfDayProperties(@Query("api_key") apiKey: String): PictureOfDay
+
+    @GET("neo/rest/v1/feed")
+    fun getAsteroidData(
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("api_key") apiKey: String
+    ): Call<String>
 }
 
 object NasaApi {
     val retrofitService: NasaApiService by lazy {
         retrofit.create(NasaApiService::class.java)
+    }
+
+    val retrofitService2: NasaApiService by lazy {
+        retrofit2.create(NasaApiService::class.java)
     }
 }
--- AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -5,29 +5,46 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.NasaApi
 import com.udacity.asteroidradar.api.NasaApiService
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainViewModel : ViewModel() {
     private val API_KEY ="3DqKevLhsid80oZMvqwGETPyOY8pK5cVNuUlRXr8"
+    private val START_DATE ="2022-01-03"
+    private val END_DATE = "2022-01-10"
 
-    private val _response = MutableLiveData<String>()
+    // The internal MutableLiveData String that stores the most recent response status
+    private var _status = MutableLiveData<String>()
+
+    // The external immutable LiveData for the status String
+    val status: LiveData<String>
+        get() = _status
+
+    private var _response = MutableLiveData<String>()
     val response: LiveData<String>
         get() = _response
 
-    private val _propertyOfPictureOfDay = MutableLiveData<PictureOfDay>()
+    private var _propertyOfPictureOfDay = MutableLiveData<PictureOfDay>()
     val propertyOfPictureOfDay: LiveData<PictureOfDay>
         get() = _propertyOfPictureOfDay
+
+    private var _asteroidData = MutableLiveData<MutableList<Asteroid>>()
+    val asteroidData: LiveData<MutableList<Asteroid>>
+        get() = asteroidData
 
 
     init {
         getNasaPictureOfDay()
+        getAsteroidData()
     }
 
     private fun getNasaPictureOfDay() {
@@ -40,4 +57,22 @@ class MainViewModel : ViewModel() {
             }
         }
     }
+
+    private fun getAsteroidData() {
+        NasaApi.retrofitService2.getAsteroidData(START_DATE, END_DATE, API_KEY).enqueue(object: Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+                val parseAsteroidsJsonResult = parseAsteroidsJsonResult(JSONObject(_response.value))
+                Log.i("MarsViewModel", "size is: ${parseAsteroidsJsonResult.size}")
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+
+        })
+        _response.value = "Set the Mars API Response here!"
+        Log.i("MarsViewModel", "last ${_response.value}")
+    }
+
 }

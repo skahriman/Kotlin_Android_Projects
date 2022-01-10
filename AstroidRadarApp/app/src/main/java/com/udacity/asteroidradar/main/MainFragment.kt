ed39a16ed0b75a4f30c2b88539052732efacbc04
SefakKahriman <sefak.kahriman@gsa.gov>
2022-01-10 14:16:55 -0500

    able to see recyclerview items in UI successfully

--- AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ AstroidRadarApp/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.udacity.asteroidradar.Asteroid
@@ -14,19 +15,10 @@ import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-//    val list = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-    val list = listOf(Asteroid(
-    1, "code1",
-    "2022-01-01",
-    1.0,
-    1.0,
-    1.0,
-    1.0,
-    false ))
-
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
+    private lateinit var myAdapter: MyAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -37,21 +29,21 @@ class MainFragment : Fragment() {
 
         setHasOptionsMenu(true)
 
-        val adapter = MyAdapter(list)
+        viewModel.asteroidData.observe(viewLifecycleOwner, Observer { response ->
+            myAdapter = viewModel.asteroidData.value?.let { MyAdapter(it) }!!
+            binding.asteroidRecycler.adapter = myAdapter
+            //for listener
+            myAdapter.setOnItemClickListener(object : MyAdapter.MyClickListener {
+                override fun onItemClick(position: Int) {
+                    Log.i("MainFragment", "${position}")
+                    Toast.makeText(context, "You clicked ", Toast.LENGTH_LONG).show();
+                }
 
-        binding.asteroidRecycler.adapter = adapter
-
-        //for listener
-        adapter.setOnItemClickListener(object : MyAdapter.MyClickListener {
-            override fun onItemClick(position: Int) {
-                Log.i("MainFragment", "${position}")
-                Toast.makeText(context, "You clicked ", Toast.LENGTH_LONG).show();
-            }
-
+            })
+            // for listener
         })
-        // for listener
 
-        binding.asteroidRecycler.layoutManager = LinearLayoutManager(context)
+
 
         return binding.root
     }
